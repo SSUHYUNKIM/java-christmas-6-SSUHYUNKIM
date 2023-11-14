0c9d10c9efaba672d539b8ac8cfd9e373fd57e60
SSUHYUNKIM <yellosuhyun@naver.com>
2023-11-15 03:07:37 +0900

    feat: 입력값이 숫자인지 판별하는 예외 추가

--- src/main/java/christmas/util/Validator.java
+++ src/main/java/christmas/util/Validator.java
@@ -5,7 +5,22 @@ import static christmas.util.ErrorMessage.*;
 
 public class Validator {
 
-    private String removeSpacing(String inputValue) {
+    public static void validateVisitDate(String inputValue) {
+        checkNull(inputValue);
+        inputValue = removeSpacing(inputValue);
+        checkExistOfValue(inputValue);
+        checkNegative(inputValue);
+        checkDateRange(inputValue);
+    }
+    public static void checkIsInteger(String inputValue) {
+        try {
+            Integer.parseInt(inputValue);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NUMERIC_INPUT_ERROR.getMessage());
+        }
+    }
+
+    private static String removeSpacing(String inputValue) {
         return inputValue.replaceAll(" ", "");
     }
 
@@ -20,13 +35,17 @@ public class Validator {
         }
     }
 
-    private static void checkNegative(int inputValue) {
-        if (inputValue < 0) {
+    private static void checkNegative(String inputValue) {
+        checkIsInteger(inputValue);
+        int num = Integer.parseInt(inputValue);
+        if (num < 0) {
             throw new IllegalArgumentException(NEGATIVE_NUMBER_ERROR.getMessage());
         }
     }
 
-    private static void checkDateRange(int date) {
+    private static void checkDateRange(String inputValue) {
+        checkIsInteger(inputValue);
+        int date = Integer.parseInt(inputValue);
         if (date > MAX_DATE_NUMBER || date < MIN_DATE_NUMBER) {
             throw new IllegalArgumentException(INVALID_DATE_ERROR.getMessage());
         }
